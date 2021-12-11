0cc016985b4c73c093e2d29d215b197ad3e4a1de
Pedro Vinicius <pedro.santos@metasix.com.br>
2021-12-11 18:12:06 -0300

    Aula 01 variaveis

---
+++ exercicios-teoricos/aula1/js/variaveis.js
@@ -0,0 +1,17 @@
+
+//let my age = "20"; não pode dar espaço
+//let let = 15;   // não pode usar let como nome de variavel pois é uma palavra reservada
+let age = 31; // let é uma variável de escopo local
+let currentYear = new Date().getFullYear();
+
+console.log(age, currentYear);
+
+age++;
+console.log(age);
+
+const points = 100;
+//points++; Não é possível alterar o valor de uma constante
+// Te amo github copilot!
+
+var score = 75; // var é uma variável global
+console.log(score);
